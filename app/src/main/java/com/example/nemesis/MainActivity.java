3694247a143499788ea11ffca9eb9996e9e948b1
Robbie <rothrockre@appstate.edu>
2019-03-23 14:24:04 -0400

    work on login page

--- app/src/main/java/com/example/nemesis/MainActivity.java
+++ app/src/main/java/com/example/nemesis/MainActivity.java
@@ -1,57 +1,94 @@
 package com.example.nemesis;
 
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-
-import android.app.Activity;
-
+import android.util.Log;
 import android.view.View;
-
-import android.widget.Button;
-import android.widget.Toast;
-
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-
-    private Button loginButton;
-    private EditText loginUsername, loginPassword;
-
-    private int counter = 3;
-
+    private EditText etUserEmail;
+    private EditText etPassword;
+    public String username;
+    private String password;
+    String storedPassword;
+    Context context=this;
+    LoginDataBaseAdapter loginDataBaseAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loginButton = (Button)findViewById(R.id.loginButton);
-        loginPassword = (EditText)findViewById(R.id.loginPassword);
-        loginUsername = (EditText)findViewById(R.id.loginUsername);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(loginUsername.getText().toString().toLowerCase().equals("admin") &&
-                        loginPassword.getText().toString().equals("admin")) {
-                    openHomepage();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",
-                            Toast.LENGTH_SHORT).show();
-
-                    counter--;
-                    if(counter == 0) {
-                        loginButton.setEnabled(false);
+        // create the instance of Databse
+        loginDataBaseAdapter=new LoginDataBaseAdapter(getApplicationContext());
+        etUserEmail = (EditText) findViewById(R.id.Email);
+        etPassword = (EditText) findViewById(R.id.Password);
+    }
+    public void SignIN(View view) {
+        try {
+            loginDataBaseAdapter = loginDataBaseAdapter.open();
+            username = etUserEmail.getText().toString();
+            password = etPassword.getText().toString();
+            if (username.equals("") || password.equals("")) {
+                AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setTitle("ALERT!");
+                alertDialog.setMessage("Fill All Fields");
+                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
                     }
+                });
+                alertDialog.show();
+            }
+            // fetch the Password form database for respective user name
+            if (!username.equals("")) {
+                storedPassword = loginDataBaseAdapter.getSinlgeEntry(username);
+                // check if the Stored password matches with Password entered by user
+                if (password.equals(storedPassword)) {
+                    Intent intent1 = new Intent(MainActivity.this, Homepage.class);
+                    startActivity(intent1);
+                    // finish();
+                }
+                else
+                {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                    alertDialog.setTitle("ALERT!");
+                    alertDialog.setMessage("Incorrect Username OR Password");
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    alertDialog.show();
                 }
             }
-        });
-
+        }
+        catch (Exception ex)
+        {
+            Log.e("Error", "error login");
+        }
     }
-
-    public void openHomepage() {
-        Intent intent = new Intent(this, Homepage.class);
+    public void SignUP(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, SignUp.class);
         startActivity(intent);
     }
+    @Override
+    protected void onDestroy()
+    {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
+    }
 
+
+
+
+    public void openHomepage(){
+        Intent intent=new Intent(this,Homepage.class);
+        startActivity(intent);
+    }
 }
