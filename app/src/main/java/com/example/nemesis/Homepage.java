0dcce2731ea5f5fb132ef6bc3155b62a5b3aa7c9
Robbie <rothrockre@appstate.edu>
2019-03-30 15:08:16 -0400

    Got the titles for Homepage news articles

--- app/src/main/java/com/example/nemesis/Homepage.java
+++ app/src/main/java/com/example/nemesis/Homepage.java
@@ -1,18 +1,33 @@
 package com.example.nemesis;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.*;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.view.GravityCompat;
 import android.view.View;
+import android.webkit.HttpAuthHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Homepage extends AppCompatActivity {
 
+    String news_url;
 
     private DrawerLayout drawerLayout;
 
@@ -29,6 +44,12 @@ public class Homepage extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
+        ///////////////////////////////////////////////
+        news_url = "https://newsapi.org/v2/everything?q=fox-sports&apiKey=0b23857f53cb4d00aee3ad9e141222cd";
+
+
+        new Homepage.AsyncHttpTask().execute(news_url);
+        ////////////////////////////////////////////////
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
@@ -109,4 +130,66 @@ public class Homepage extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public class AsyncHttpTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                String response = streamToString(urlConnection.getInputStream());
+                parseResult(response);
+                return result;
+
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+    }
+
+    String streamToString(InputStream stream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        String data;
+        String result ="";
+
+        while ((data = bufferedReader.readLine()) != null) {
+
+            result += data;
+        }
+
+        if(null != stream) {
+            stream.close();
+        }
+
+        return result;
+    }
+
+    private void parseResult(String result) {
+        JSONObject response = null;
+        try {
+            response = new JSONObject(result);
+            JSONArray articles = response.optJSONArray("articles");
+
+            for(int i = 0; i < articles.length(); i++) {
+                JSONObject article = articles.getJSONObject(i);
+                String title = article.optString("title");
+                Log.i("Titles", title);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONArray articles = response.optJSONArray("articles");
+
+    }
 }
